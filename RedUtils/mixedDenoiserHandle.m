94a702ec71559d56ca4542894abfe14342ce92fc
SebastianMerino <109495963+SebastianMerino@users.noreply.github.com>
2026-01-14 09:41:46 -0500

    added denoiser handle

---
+++ RedUtils/mixedDenoiserHandle.m
@@ -0,0 +1,13 @@
+function f = mixedDenoiserHandle(m,n,kernel,lambda)
+    function fx = mixedDenoiser(x)
+    x = reshape(x,[m,n,2]);
+    x1 = x(:,:,1);
+    x2 = x(:,:,2);
+    x1 = medfilt2(x1, [kernel(1),kernel(1)],'symmetric');
+    x2 = sign(x2).*max(0,abs(x2)-lambda); % L1-norm denoising
+    % x2 = x2/(1+lambda); % L2-norm denoising
+    fx= cat(3,x1,x2);
+    fx = fx(:);
+    end
+    f = @mixedDenoiser;
+end
